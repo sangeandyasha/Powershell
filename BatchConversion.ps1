67f840e7376c0284b25a31bfeeb599701f9478cd
sangeandyasha <silverheirophant@gmail.com>
2021-09-01 15:34:03 -0700

    Add files via upload

---
+++ BatchConversion.ps1
@@ -0,0 +1,13 @@
+﻿#Batch Conversion for Filetype With Inputs
+
+$ExitCode = $False
+Do{
+    $target = Read-Host -Prompt 'Enter the target drive path'
+    $OGFile = Read-Host -Prompt 'Input OG File extension'
+    $Nfile = Read-Host -Prompt 'Input New File Extension'
+    Get-ChildItem $target -filter *$OGfile | Rename-Item -NewName {[System.IO.Path]::ChangeExtension($_.Name, "$Nfile")}
+    $Result = Read-Host -Prompt 'Continue? Yes or No'
+    If ($Result -eq "No"){
+        $ExitCode = $True
+    }
+} Until ($ExitCode)
